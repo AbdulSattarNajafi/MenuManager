9151ef643a16bc75cb5d7c3371d856864ec01d9e
AbdulSattarNajafi <66928318+AbdulSattarNajafi@users.noreply.github.com>
2021-01-15 19:43:55 +0700

    colors adjusted

--- components/active_offers/activate_offer/subcmp_scripts.js
+++ components/active_offers/activate_offer/subcmp_scripts.js
@@ -5,4 +5,6 @@
 //Load breakfast menu by default
 //load_module('addtotable_placeholder', 'regular_menu/breakfast_menu/mod_addto_table');
 //load_module('results_table_placeholder', 'regular_menu/breakfast_menu/mod_results_table');
-$('.date input').datetimepicker();
+$('.date input').datetimepicker({
+  format : "DD MMM YY           HH:mm",
+});
